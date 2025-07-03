7c04b28d2cb378fc14b3d3f4a0c401fa3d9af68d
valaparimal <valaparimal2245@gmail.com>
2025-07-03 16:20:10 +0530

    Update Day2 Angular project

--- Day2/Day2_Login_Form/src/app/app.routes.ts
+++ Day2/Day2_Login_Form/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:LoginFormComponent
+    },
+    {
+        path:'home',
+        component:HomeComponent
+    }
+];
--- Day2/Day2_Login_Form/src/app/login-form/login-form.component.ts
+++ Day2/Day2_Login_Form/src/app/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -11,11 +12,16 @@ import { FormsModule } from '@angular/forms';
 })
 export class LoginFormComponent {
   user_email="";
-  user_password=""
+  user_password="";
+
+  constructor(private router:Router){};
+
   submit() {
     console.log("Email : "+this.user_email);
     console.log("Password : "+this.user_password);
     this.user_email="";
     this.user_password="";
+
+    this.router.navigate(['/home']);
   }
 }
